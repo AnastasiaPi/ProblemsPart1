5904f7a620760fad69c4abc452d137b2e0c2a1a3
 Anastasia Pidgurska <anastasiapidgurska@MacBook-Pro-Anastasia.local>
2019-11-09 17:08:19 +0200

    F

--- PolindromPack/Polindrom.java
+++ PolindromPack/Polindrom.java
@@ -15,45 +15,48 @@ public class Polindrom {
         String Half;
         for (int musk = 1; musk < HALF_NUM; musk += 2) {
             Half = Integer.toBinaryString(musk);
-            toStrNum(Half);
+            String reversedStr = new StringBuilder(Half).reverse().toString();
+            System.out.println(Integer.parseInt(reversedStr + Half, 2));
+            System.out.println(Integer.parseInt(reversedStr + Zero + Half, 2));
+            System.out.println(Integer.parseInt(reversedStr + One + Half, 2));
+            Count = Count + 3;
         }
     }
-
     private static void findNegPolindrom() {
-        char[] negativePolindromes = new char[16];
-        negativePolindromes[0] = One;
+        char[] negArray = new char[16];
+        negArray[0] = One;
         for (int j = 1; j < 16; j++) {
-            negativePolindromes[j] = Zero;
+            negArray[j] = Zero;
         }
-        String bitNegativeNumber;
+        String NegativeNumber;
         String half;
         int k = 15;
         for (int i = 32767; i > 0; i--) {
-            if (negativePolindromes[k] != One) {
-                negativePolindromes[k] = One;
+            if (negArray[k] != One) {
+                negArray[k] = One;
             }
             else {
                 k = 15;
                 while (true) {
-                    if (negativePolindromes[k] == One) {
-                        negativePolindromes[k] = Zero;
+                    if (negArray[k] == One) {
+                        negArray[k] = Zero;
                         k--;
                         continue;
                     } else {
-                        negativePolindromes[k] = One;
+                        negArray[k] = One;
                         break;
                     }
                 }
             }
 
-            half = new String(negativePolindromes);
-            bitNegativeNumber = half + new StringBuilder(half).reverse().toString();
-            showInt(bitNegativeNumber);
+            half = new String(negArray);
+            NegativeNumber = half + new StringBuilder(half).reverse().toString();
+            showInt(NegativeNumber);
         }
     }
 
-    public static void showInt(String bitNegativeNumber) {
-        char[] charArr = bitNegativeNumber.toCharArray();
+    public static void showInt(String Neg) {
+        char[] charArr = Neg.toCharArray();
 
         for (int i = 0; i < 32; i++) {
             if (charArr[i] == One) {
@@ -82,13 +85,7 @@ public class Polindrom {
 
     }
 
-    public static void toStrNum(String str) {
-        String reversedStr = new StringBuilder(str).reverse().toString();
-        System.out.println(Integer.parseInt(reversedStr + str, 2));
-        System.out.println(Integer.parseInt(reversedStr + Zero + str, 2));
-        System.out.println(Integer.parseInt(reversedStr + One + str, 2));
-        Count = Count + 3;
-    }
+
 
 
 }
